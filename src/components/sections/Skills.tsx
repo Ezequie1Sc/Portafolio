3a7075648e893852a277ee6d618039ca3b81b729
Ezequiel <ezequielsc017@gmail.com>
2026-04-20 20:10:33 -0600

    hero and skills

--- src/components/sections/Skills.tsx
+++ src/components/sections/Skills.tsx
@@ -21,7 +21,7 @@ const Skills = () => {
               <div className="about-me-image-container">
                 <div className="about-me-image-wrapper">
                   <img 
-                    src="/face.jpg" 
+                    src="face.webp" 
                     alt="Profile" 
                     className="about-me-image"
                   />
